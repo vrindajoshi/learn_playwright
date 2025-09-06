451caa4fb1408981bde74c09b9955ee95560cd3c
Vrinda Joshi <vrinda.joshi@hotmail.com>
2025-09-06 18:22:18 -0400

    logoTest

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -4,5 +4,13 @@ test("Open home page and verify title", async ({page}) => {
     await page.goto("https://www.fifa.com/");
 
     await expect(page).toHaveTitle('FIFA | The Home of Football'); // use await because you are using promises (objects)
-    
+
+})
+
+test("Verify logo is visible and has the alt title", async ({page}) => {
+    await page.goto("https://www.fifa.com/");
+
+    const logo =  page.locator('[href="/en/home"]')
+
+    await expect(logo).toBeVisible();
 })
